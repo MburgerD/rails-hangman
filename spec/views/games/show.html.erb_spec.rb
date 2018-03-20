5bb81333111e833ddb28d2978061345335be83b5
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-21 12:52:48 +1300

    games #show view spec to expect form with "guesses" input field

--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "games/show", type: :view do
   before(:each) do
     @game = assign(:game, Game.create!(
-      :word => "Word",
-      :guesses => "",
-      :lives => 2
+                            :word => "Word",
+                            :guesses => "",
+                            :lives => 2
     ))
   end
 
@@ -14,5 +14,13 @@ RSpec.describe "games/show", type: :view do
     expect(rendered).to match(/Word/)
     expect(rendered).to match(//)
     expect(rendered).to match(/2/)
+  end
+
+  it "renders show game form" do
+    render
+
+    assert_select "form[action=?][method=?]", "/games/#{@game.id}", "post" do
+      assert_select "input[name=?]", "game[guesses]"
+    end
   end
 end
