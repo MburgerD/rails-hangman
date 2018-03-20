adcfd0291137d0c42d867e3d66d42f64922dd30c
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-21 12:08:12 +1300

    update game/edit view spec to expect correct form

--- spec/views/games/edit.html.erb_spec.rb
+++ spec/views/games/edit.html.erb_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe "games/edit", type: :view do
   before(:each) do
     @game = assign(:game, Game.create!(
-      :word => "MyString",
-      :guesses => "",
-      :lives => 1
+                            :word => "MyString",
+                            :lives => 1
     ))
   end
 
@@ -13,11 +12,7 @@ RSpec.describe "games/edit", type: :view do
     render
 
     assert_select "form[action=?][method=?]", game_path(@game), "post" do
-
       assert_select "input[name=?]", "game[word]"
-
-      assert_select "input[name=?]", "game[guesses]"
-
       assert_select "input[name=?]", "game[lives]"
     end
   end
