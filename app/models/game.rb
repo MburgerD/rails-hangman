33c2f8e19ffc0e6858de5679207d736d51bb87a4
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-21 09:59:06 +1300

    add validation for Game.lives

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,7 @@ class Game < ApplicationRecord
   validates :word, presence: true,
                    length: { maximum: 20 },
                    format: { with: /\A[a-zA-Z]+\z/ }
+  validates :lives, numericality: { only_integer: true,
+                                    less_than_or_equal_to: 10,
+                                    greater_than: 0 }
 end
