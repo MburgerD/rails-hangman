3dd8f106bcc8d6fa5a62b115625caeb591cee44d
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-20 14:13:10 +1300

    write hangman index view test to check for correct title

--- spec/views/hangman/index.html.erb_spec.rb
+++ spec/views/hangman/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "hangman/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "displays the hangman heading" do
+    render
+    expect(rendered).to include("Let's play HANGMAN!")
+  end
 end
