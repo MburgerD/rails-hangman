83c196b70d9618014b38f0b0d7155ae728e8aa4e
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-21 17:11:46 +1300

    spec for game/show view asserting only diplays guessed letters

--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -4,16 +4,9 @@ RSpec.describe "games/show", type: :view do
   before(:each) do
     @game = assign(:game, Game.create!(
                             :word => "Word",
-                            :guesses => "abc",
+                            :guesses => "r",
                             :lives => 2
     ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Word/)
-    expect(rendered).to match(/abc/)
-    expect(rendered).to match(/2/)
   end
 
   it "renders show game form" do
@@ -22,5 +15,10 @@ RSpec.describe "games/show", type: :view do
     assert_select "form[action=?][method=?]", "/games/#{@game.id}", "post" do
       assert_select "input[name=?]", "game[guess]"
     end
+  end
+
+  it "shows only letters in the word which have been guessed" do
+    render
+    expect(rendered).to match(/--r-/)
   end
 end
