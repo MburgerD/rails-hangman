f28dcb714b9aba378832a4a30f4433421c5af0a0
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-21 15:41:39 +1300

    create "guess" attribute on game model and validate, post "guess" from view

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,13 @@
 class Game < ApplicationRecord
+  attr_accessor :guess
+
   validates :word, presence: true,
                    length: { maximum: 20 },
                    format: { with: /\A[a-zA-Z]+\z/ }
   validates :lives, numericality: { only_integer: true,
                                     less_than_or_equal_to: 10,
                                     greater_than: 0 }
-  validates :guesses, presence: true, on: :update,
-                      length: { maximum: 1 },
-                      format: { with: /[a-zA-Z]/, message: "must be a letter" }
+  validates :guess, presence: true, on: :update,
+                    length: { maximum: 1 },
+                    format: { with: /[a-zA-Z]/, message: "must be a letter" }
 end
