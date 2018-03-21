826c6cef3668f019a5f6b62d1372c70cdded5268
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-21 16:42:18 +1300

    update game controller spec update action

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -24,17 +24,11 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe GamesController, type: :controller do
-
   # This should return the minimal set of attributes required to create a valid
   # Game. As you add validations to Game, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:valid_attributes) { { "word" => "foo", "lives" => 5 } }
+  let(:invalid_attributes) { { "word" => 123, "lives" => 500 } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -64,14 +58,6 @@ RSpec.describe GamesController, type: :controller do
     end
   end
 
-  describe "GET #edit" do
-    it "returns a success response" do
-      game = Game.create! valid_attributes
-      get :edit, params: {id: game.to_param}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
   describe "POST #create" do
     context "with valid params" do
       it "creates a new Game" do
@@ -96,28 +82,28 @@ RSpec.describe GamesController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:new_attributes) { { "guess" => "a" } }
 
       it "updates the requested game" do
         game = Game.create! valid_attributes
         put :update, params: {id: game.to_param, game: new_attributes}, session: valid_session
         game.reload
-        skip("Add assertions for updated state")
+        expect(game.guesses).to eq "a"
       end
 
       it "redirects to the game" do
         game = Game.create! valid_attributes
-        put :update, params: {id: game.to_param, game: valid_attributes}, session: valid_session
+        put :update, params: {id: game.to_param, game: new_attributes}, session: valid_session
         expect(response).to redirect_to(game)
       end
     end
 
     context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
+      let(:new_invalid_attributes) { { "guess" => 123 } }
+
+      it "returns a success response (i.e. to display the 'show' template)" do
         game = Game.create! valid_attributes
-        put :update, params: {id: game.to_param, game: invalid_attributes}, session: valid_session
+        put :update, params: {id: game.to_param, game: new_invalid_attributes}, session: valid_session
         expect(response).to be_success
       end
     end
@@ -137,5 +123,4 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(games_url)
     end
   end
-
 end
