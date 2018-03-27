2d73c2cd79f0ee66759f2df39800f7b7d7fe3605
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-03-27 17:11:24 +1300

    cucumber tests for creating new game

---
+++ features/new_game.feature
@@ -0,0 +1,28 @@
+Feature: New Game
+	As a gamer
+	I want to see a heading when I visit the site
+	I want to a link to get to the games page
+	I want to be able to create a new game
+
+Scenario: User sees the welcome message
+	When I am on the homepage
+	Then I should see the welcome message
+	Then I should see the link to games
+
+Scenario: User clicks the link to games on the home page
+	When I am on the homepage
+	And I click on the link to games
+	Then I should go the games page
+	Then I should see a link to new game
+
+Scenario: User clicks the link to new game
+	When I am on the games page
+	When I click on the new game link
+	Then I should go the new game page
+
+Scenario: User creates a new game
+	When I am on the new game page
+	And I enter a word
+	Then I click create game
+	Then I have created a game
+	And I should go the game show page
---
+++ features/step_definitions/new_game_steps.rb
@@ -0,0 +1,56 @@
+When(/^I am on the homepage$/) do
+  visit root_path
+end
+
+When(/^I am on the games page$/) do
+  visit games_path
+end
+
+When(/^I am on the new game page$/) do
+  visit new_game_path
+end
+
+Then(/^I should see the welcome message$/) do
+  expect(page).to have_content("Let's play HANGMAN!")
+end
+
+Then(/^I should see the link to games$/) do
+  have_link("Go to games")
+end
+
+And(/^I click on the link to games$/) do
+  click_link("Go to games")
+end
+
+Then(/^I should go the games page$/) do
+  expect(page).to have_current_path("/games")
+end
+
+Then(/^I should see a link to new game$/) do
+  have_link("New Game")
+end
+
+When(/^I click on the new game link$/) do
+  click_link("New Game")
+end
+
+Then(/^I should go the new game page$/) do
+  expect(page).to have_current_path("/games/new")
+end
+
+And(/^I enter a word$/) do
+  fill_in('Word', with: 'FooBar')
+end
+
+Then(/^I click create game$/) do
+  click_on('Create Game')
+end
+
+Then(/^I have created a game$/) do
+  expect(Game.all.count).to eq 1
+end
+
+And(/^I should go the game show page$/) do
+  game_id = Game.first.id
+  expect(page).to have_current_path("/games/#{game_id}")
+end
