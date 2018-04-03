a6d52294d559c4e8dc4eee22b659e26d859ab99e
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-04-04 11:14:35 +1200

    implementation of GuessesController

---
+++ app/controllers/guesses_controller.rb
@@ -0,0 +1,22 @@
+class GuessesController < ApplicationController
+  def create
+    respond_to do |format|
+      @game = Game.find(params[:game_id])
+      @guess = @game.guesses.create(guess_params)
+      @game.reload
+      if @guess.valid?
+        format.html { redirect_to @game }
+        format.json { render :show, status: :created, location: @game }
+      else
+        format.html { render 'games/show' }
+        format.json { render json: @guess.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def guess_params
+    params.require(:guess).permit(:letter)
+  end
+end
