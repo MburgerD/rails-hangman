38663a26425fec04e77b7940741ecd833121d452
Max Doesburg <maxd@Maxs-MacBook-Pro.local>
2018-04-03 17:00:14 +1200

    add validation to guesses

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,8 @@
 class Guess < ApplicationRecord
   belongs_to :game
+
+  validates :letter, presence: true,
+                     uniqueness: { scope: :game_id },
+                     length: { maximum: 1 },
+                     format: { with: /[a-zA-Z]/, message: "must be a letter" }
 end
